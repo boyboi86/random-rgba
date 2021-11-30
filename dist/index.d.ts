cef9a66e4725656bc218818a5bf58487e991457a
Dave Held <dav3h3ld@gmail.com>
2021-11-30 13:06:21 -0800

    chore: add typescript and TS declaration

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export = randomRgba;
+/**
+ * Generate a string in the CSS format `rgba(r, g, b, a)` with random values.
+ * @param {number} alpha - optional opacity value: defaults to a random percentage
+ */
+declare function randomRgba(alpha: number): string;
